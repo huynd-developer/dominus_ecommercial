f2456d667a7b21a4d003d39dd2656311b9596de2
huynd-developer <nguyhuy1408@gmail.com>
2026-06-25 10:39:08 +0700

    Update lại be phần auth author

--- Backend/src/main/java/org/example/datn_sd69/modules/auth/dto/request/RegisterRequest.java
+++ Backend/src/main/java/org/example/datn_sd69/modules/auth/dto/request/RegisterRequest.java
@@ -12,14 +12,17 @@ import lombok.Setter;
 public class RegisterRequest {
 
     @NotBlank(message = "Tên không được để trống")
+    @Pattern(regexp = "^[\\p{L}\\s]+$", message = "Tên không hợp lệ (Không được chứa số hoặc ký tự đặc biệt)")
+    @Size(max = 50, message = "Tên không được vượt quá 50 ký tự")
     private String name;
 
     @NotBlank(message = "Email không được để trống")
     @Email(message = "Email không đúng định dạng")
+    @Size(max = 100, message = "Email không được vượt quá 100 ký tự")
     private String email;
 
     @NotBlank(message = "Mật khẩu không được để trống")
-    @Size(min = 6, message = "Mật khẩu phải từ 6 ký tự trở lên")
+    @Size(min = 6, max = 50, message = "Mật khẩu phải từ 6 đến 50 ký tự")
     private String password;
 
     @NotBlank(message = "Số điện thoại không được để trống")
