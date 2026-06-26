78f9b898b76445f96ce9cfb5188b30913d434230
Hyeu1802 <trunghieu0811207@gmail.com>
2026-06-26 21:58:27 +0700

    Update be

---
+++ Backend/src/main/java/org/example/datn_sd69/modules/auth/dto/response/AuthResponse.java
@@ -0,0 +1,14 @@
+//package org.example.datn_sd69.modules.auth.dto.request.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//public class AuthResponse {
+//    private String token;
+//    private String role;
+//    private String name;
+//}
