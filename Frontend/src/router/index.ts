5761990aea0707ee2540929d85e829011e9a947f
Kidng03 <nguyenthanhdan0310@gmail.com>
2026-06-30 00:35:37 +0700

    Loi router

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -66,7 +66,7 @@ const routes: Array<RouteRecordRaw> = [
 {
         path: "pos",
         name: "AdminPOS",
-        component: () => import("@/modules/pos/views/PosView.vue"),
+        component: () =>import("@/modules/pos/views/PosViews.vue"), 
         meta: {
           requiresAuth: true,
           allowedRoles: ["OWNER", "MANAGER", "CASHIER"],
